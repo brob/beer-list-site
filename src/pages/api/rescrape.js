bb78c8c3580cbdbc6a262f4e2ff6d323a64f5726
brob <bryanlrobinson@gmail.com>
2024-12-24 16:33:32 -0600

    Logs the url for testing

--- src/pages/api/rescrape.js
+++ src/pages/api/rescrape.js
@@ -6,7 +6,7 @@ const {parse} = pkg;
 export const prerender = false;
 
 export async function GET({params, request}) {
-
+  console.log(import.meta.env.SITE_URL + `/api/processCheckin)
     const rss = await parse('https://untappd.com/rss/user/glrob54?key=22bf8950a97512ac2b5da8bb7617ab76')
     // console.log(rss)
 
